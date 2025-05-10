7e5ba6aa3d9b4db17ee785843c92ee6f17f56a31
ALDRICH012 <168687755+ALDRICH012@users.noreply.github.com>
2025-05-10 12:04:19 -0300

    Update urls.py

--- HTML CURSO/urls.py
+++ HTML CURSO/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import donor_list, add_donor, add_donation
+from .views import donor_list, add_donor, edit_donor, delete_donor, add_donation
 
 app_name = 'doacoes'
 
 urlpatterns = [
     path('', donor_list, name='donor_list'),
     path('doador/novo/', add_donor, name='add_donor'),
+    path('doador/editar/<int:pk>/', edit_donor, name='edit_donor'),
+    path('doador/excluir/<int:pk>/', delete_donor, name='delete_donor'),
     path('doacao/nova/', add_donation, name='add_donation'),
 ]
