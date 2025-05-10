56ed9b1546464fb2af1e576d5426434f81e713ce
ALDRICH012 <168687755+ALDRICH012@users.noreply.github.com>
2025-05-10 12:07:55 -0300

    Update forms.py

--- HTML CURSO/forms.py
+++ HTML CURSO/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .models import Donor, Donation
+import re
 
 class DonorForm(forms.ModelForm):
     class Meta:
@@ -16,53 +19,65 @@ class DonorForm(forms.ModelForm):
             }),
             'phone': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': '+999999999'
+                'placeholder': '+5511999999999'
             })
         }
 
     def clean_phone(self):
         phone = self.cleaned_data.get('phone')
         if not phone.startswith('+'):
-            raise forms.ValidationError('O número de telefone deve começar com +')
-        return phone
+            raise forms.ValidationError('O número deve começar com "+" (ex: +5511999999999)')
+        
+        # Remove tudo exceto dígitos e o "+"
+        cleaned_phone = '+' + re.sub(r'[^\d]', '', phone[1:])
+        
+        if len(cleaned_phone) < 10:
+            raise forms.ValidationError('Número muito curto (mínimo 10 dígitos)')
+            
+        return cleaned_phone
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise forms.ValidationError('Formato de e-mail inválido')
+        
+        if Donor.objects.filter(email=email).exists():
+            raise forms.ValidationError('E-mail já cadastrado')
+        return email
 
 class DonationForm(forms.ModelForm):
     class Meta:
         model = Donation
         fields = ['donor', 'amount', 'description', 'status']
         widgets = {
-            'donor': forms.Select(attrs={
-                'class': 'form-select'
-            }),
+            'donor': forms.Select(attrs={'class': 'form-select'}),
             'amount': forms.NumberInput(attrs={
                 'class': 'form-control',
                 'placeholder': '0.00',
                 'step': '0.01',
-                'min': '0'
+                'min': '0.01'  # Valor mínimo de 1 centavo
             }),
             'description': forms.Textarea(attrs={
                 'class': 'form-control',
-                'placeholder': 'Descrição da doação',
                 'rows': 3
             }),
-            'status': forms.Select(attrs={
-                'class': 'form-select'
-            })
+            'status': forms.Select(attrs={'class': 'form-select'})
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Ordena os doadores por nome
         self.fields['donor'].queryset = Donor.objects.all().order_by('name')
         
-        # Adiciona classes Bootstrap aos labels
-        for field_name, field in self.fields.items():
-            field.label_suffix = ''
-            if field_name == 'amount':
-                field.label = 'Valor (R$)'
-            elif field_name == 'donor':
-                field.label = 'Doador'
-            elif field_name == 'description':
-                field.label = 'Descrição'
-            elif field_name == 'status':
-                field.label = 'Status' 
+        # Labels customizados
+        self.fields['amount'].label = 'Valor (R$)'
+        self.fields['donor'].label = 'Doador'
+        self.fields['description'].label = 'Descrição'
+        self.fields['status'].label = 'Status'
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount <= 0:
+            raise forms.ValidationError('O valor deve ser positivo')
+        return amount
